ca61008d9813d07444e42cb70dd82be06f8f2fa0
antoniorusso2 <russoantonio303@outlook.it>
2025-04-29 18:27:39 +0200

    dishes page responsiveness

--- src/pages/DishesPage.jsx
+++ src/pages/DishesPage.jsx
@@ -1,12 +1,11 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import placeholder from "../assets/placeholder.png";
+// .env api base url for dishes index
+const defaultApiUrl = import.meta.env.VITE_API_URL + "dishes";
+const defaultImgUrl = import.meta.env.VITE_API_IMG_URL;
 
 export default function DishesPage() {
-  // .env api base url for dishes index
-  const defaultApiUrl = import.meta.env.VITE_API_URL + "dishes";
-  const defaultImgUrl = import.meta.env.VITE_API_IMG_URL;
-
   const [dishes, setDishes] = useState([]);
 
   const getDishes = async () => {
@@ -26,28 +25,23 @@ export default function DishesPage() {
 
   return (
     <div className="container mt-3">
-      <div className="row g-3">
-        {dishes.map(
-          (dish) => (
-            console.log(defaultImgUrl + dish.image),
-            (
-              <div className="col-4" key={dish.id}>
-                <div className="card h-100">
-                  <img
-                    src={defaultImgUrl + dish.image}
-                    onError={(e) => (e.target.src = placeholder)}
-                    className="card-img-top"
-                    alt={dish.name}
-                  />
-                  <div className="card-body">
-                    <h5 className="card-title">{dish.name}</h5>
-                    <p className="card-text">{dish.description}</p>
-                  </div>
-                </div>
+      <div className="row  row-cols-1 row-cols-md-2 row-cols-lg-4 g-3">
+        {dishes.map((dish) => (
+          <div className="col-12" key={dish.id}>
+            <div className="card h-100">
+              <img
+                src={defaultImgUrl + dish.image}
+                onError={(e) => (e.target.src = placeholder)}
+                className="card-img-top"
+                alt={dish.name}
+              />
+              <div className="card-body">
+                <h5 className="card-title">{dish.name}</h5>
+                <p className="card-text">{dish.description}</p>
               </div>
-            )
-          )
-        )}
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
